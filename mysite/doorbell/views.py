df1ca2d3a24f8e410536ebea4869923a9d5a06be
Rae <raehalamish@gmail.com>
2024-02-28 20:30:52 +0000

    remove twitter

--- mysite/doorbell/views.py
+++ mysite/doorbell/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth import login, logout
 from django.contrib.auth import authenticate
 from django.shortcuts import render, redirect
-from twitter.models import User
+from django.contrib.auth.models import User
 from django.db import IntegrityError
 from .models import UserProfile, Borough, Post, Group
 
