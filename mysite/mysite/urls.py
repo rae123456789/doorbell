f908d67ac17eb19fe6cd61d75b7319364827f216
Rae <raehalamish@gmail.com>
2024-02-28 20:19:41 +0000

    removed twitter

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
-    path('twitter/', include('twitter.urls')),
+    # path('twitter/', include('twitter.urls')),
     path('', include('doorbell.urls'))
 ]
